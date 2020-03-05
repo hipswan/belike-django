9ac12fadf3f7bccf1d8390f60deded03afa48b89
hipswan <atulsingh158@gmail.com>
2020-03-05 14:16:35 +0530

    Editing url path to understand the routing

--- hipswan/urls.py
+++ hipswan/urls.py
@@ -19,9 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('travello.urls')),
+    path('travello',include('travello.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls'))
+    path('travello/accounts/',include('accounts.urls'))
 ]
 
 urlpatterns =urlpatterns + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
